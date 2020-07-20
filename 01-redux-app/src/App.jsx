578e9cd81ab526672c3d8ea23d8cc97932234e67
jason <Jason.jafari.m@gmail.com>
2020-07-20 12:05:55 -0400

    add state for application

--- 01-redux-app/src/App.jsx
+++ 01-redux-app/src/App.jsx
@@ -5,11 +5,17 @@ import './App.css'
 class App extends React.Component {
 
 
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
 
   render(){
     return(
       <div>
-        <h2>Count: 2</h2>
+        <h2>Count: {this.state.count}</h2>
         <button>Decrement</button>
         <button>Increment</button>
       </div>
