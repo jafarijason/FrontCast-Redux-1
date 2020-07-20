26175172d4659aa784e0472f2d99f3d964a9cda5
jason <Jason.jafari.m@gmail.com>
2020-07-20 12:44:27 -0400

    importe create store to main app

--- 01-redux-app/src/index.js
+++ 01-redux-app/src/index.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+
+import Counter from "./components/Counter";
+
+import counterState from "./reducers";
+
+const store = createStore(counterState)
+
+const rootElement = document.getElementById('root')
